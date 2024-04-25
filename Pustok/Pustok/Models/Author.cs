d362b19252d77af0f4ac2ba78b8ca2eb018b315f
Elchin Aghayev <Elchin@code.local>
2024-04-25 21:52:17 +0400

    project added

---
+++ Pustok/Pustok/Models/Author.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pustok.Models
+{
+    public class Author:BaseEntity
+    {
+        [MaxLength(30)]
+        [MinLength(3)]
+        [Required]
+        public string Fullname { get; set; }
+    }
+}
---
+++ Pustok/Pustok/Models/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Pustok.Models
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+    }
+}
